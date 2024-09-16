3c8201a3d8bf72605f8d6c41d339b949af818d05
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-09-16 20:27:36 +0600

    first commit

---
+++ javascriptstyle.js
@@ -0,0 +1,10 @@
+const sections=document.querySelectorAll('section');
+for(let section of sections){
+  section.style.border='2px solid steelblue';
+  section.style.marginBottom='5px';  
+  section.style.borderRadius='15px';
+  section.style.padding='7px';
+  section.style.backgroundColor='lightblue';
+  section.style.boxShadow='5px 5px 5px lightgrey';
+
+}
